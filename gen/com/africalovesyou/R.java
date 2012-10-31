a7f1c9c80eab9d03a00b0a59b669a03e1cb4b396
Pierre LaFayette <plafayet@qualcomm.com>
2012-10-30 23:00:45 -0400

    Updating conversion rates and switching from CAD to USD.

--- gen/com/africalovesyou/R.java
+++ gen/com/africalovesyou/R.java
@@ -18,6 +18,7 @@ public final class R {
         public static final int convert=0x7f020004;
         public static final int ic_launcher=0x7f020005;
         public static final int ke=0x7f020006;
+        public static final int usa=0x7f020007;
     }
     public static final class id {
         public static final int amount1=0x7f050009;
